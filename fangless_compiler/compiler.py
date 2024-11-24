bb48cb4951ae0e5bba57167163b01f92009b3e15
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2024-11-23 21:08:31 -0600

    Refactor code generation and compilation process; enhance tree traversal and formatting capabilities
    Co-authored-by: kdaniel1652 <kdaniel1652@users.noreply.github.com>

--- fangless_compiler/compiler.py
+++ fangless_compiler/compiler.py
@@ -1,21 +1,26 @@
 from pathlib import Path
 import sys
+import common
 from lexer import FanglessLexer
 from parser import FanglessParser
+from code_generator import FanglessGenerator
+import subprocess
 
 
 class FanglessCompiler:
     def __init__(self) -> None:
         self.lexer = FanglessLexer()
         self.lexer.build()
+        self.generator = FanglessGenerator()
 
-    def compile(self) -> None:
+    def compile(self) -> str:
         program_to_compile_file_name = Path(sys.argv[1])
 
         with program_to_compile_file_name.open("r") as source_file:
             content = source_file.read()
             self.parser = FanglessParser(self.lexer)
-            return self.parser.parse(content)
+            tree = self.parser.parse(content)
+            return self.generator.generate_code(tree)
 
     def test_lexer(self) -> None:
         program_to_compile_file_name = Path(sys.argv[1])
@@ -28,8 +33,35 @@ class FanglessCompiler:
 
 def main() -> None:
     compiler = FanglessCompiler()
-    compiler.compile()
-
+    generated_code = compiler.compile()
+    # format the cpp generated using clang-format
+    Path("temp.cpp").write_text(generated_code, encoding="utf-8")
+    # Run clang-format on the temporary file
+    try:
+        result = subprocess.run(  # noqa: S603 input is not from user
+            ["/usr/bin/clang-format", str(Path("temp.cpp"))],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        print(e.stderr)
+        print("Returning unformatted code")
+        if common.VERBOSE_COMPILER:
+            print("Unformatted code:")
+            print(generated_code)
+        return generated_code
+    except FileNotFoundError:
+        print("clang-format not found. Returning unformatted code")
+        if common.VERBOSE_COMPILER:
+            print("Unformatted code:")
+            print(generated_code)
+        return generated_code
+    else:
+        if common.VERBOSE_COMPILER:
+            print("Formatted code:")
+            print(result.stdout)
+        return result.stdout
 
 if __name__ == "__main__":
     main()
--- tests/ast_tests/ifs.py
+++ tests/ast_tests/ifs.py
@@ -1,10 +1,6 @@
 if 3 + 2 > 4:
-    print("hola bro")
-    if str(3 + 2) == str(6):  # dejelo ahí, no pasa nada
-        print("increíble")
-    else:
-        print("joder 2x")
+   "hola"
 elif 3 + 2 > 6:
-    print("joder")
+    "joder"
 else:
-    a = ["a"]
+    ["a"]
