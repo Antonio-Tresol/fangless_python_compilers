3bd52493914fe2151076af76f6093a46e5bf0cf3
kdaniel1652 <kennethvillalobos16@gmail.com>
2024-12-02 05:37:00 -0600

    Added testing for bool methods. With Joseph

---
+++ tests/compile/input_test_bool_methods.py
@@ -0,0 +1,14 @@
+# real
+print("real examples:")
+print(True.real())   # Outputs: 1 (real part of True, equivalent to 1)
+print(False.real())  # Outputs: 0 (real part of False, equivalent to 0)
+
+# imag
+print("\nimag examples:")
+print(True.imag())   # Outputs: 0 (imaginary part of True, which is always 0)
+print(False.imag())  # Outputs: 0 (imaginary part of False, which is always 0)
+
+# conjugate
+print("\nconjugate examples:")
+print(True.conjugate())   # Outputs: 1 (conjugate of True, equivalent to 1)
+print(False.conjugate())  # Outputs: 0 (conjugate of False, equivalent to 0)
